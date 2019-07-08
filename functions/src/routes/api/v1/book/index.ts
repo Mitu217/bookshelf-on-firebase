7592d0176250fe01f96b99fce789217a758cef72
Mitu217 <mtksk.26884um@gmail.com>
2019-07-09 01:35:25 +0900

    add query params

--- functions/src/routes/api/v1/book/index.ts
+++ functions/src/routes/api/v1/book/index.ts
@@ -2,11 +2,17 @@ import * as admin from 'firebase-admin';
 import * as express from 'express';
 
 const BOOK_COLLECTION = 'books';
+const DEFAULT_LIMIT = 20;
+const DEFAULT_PAGE = 0;
 
 const router = express();
 
 router.get('/list', async (request, response) => {
-    const snapShot = await admin.firestore().collection(BOOK_COLLECTION).offset(0).limit(20).get()
+    const page = request.query.page ? parseInt(request.query.page, 10) : DEFAULT_PAGE;
+    const limit = request.query.limit ? parseInt(request.query.limit, 10) : DEFAULT_LIMIT;
+    const offset = page * limit;
+
+    const snapShot = await admin.firestore().collection(BOOK_COLLECTION).offset(offset).limit(limit).get()
     const data = snapShot.docs.map(doc => {
         return doc.data();
     });
