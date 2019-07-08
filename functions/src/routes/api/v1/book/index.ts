ace248f3d01120bc932aeef10d6a49fc3f743062
Mitu217 <mtksk.26884um@gmail.com>
2019-07-09 02:13:15 +0900

    Add validation

--- functions/src/routes/api/v1/book/index.ts
+++ functions/src/routes/api/v1/book/index.ts
@@ -1,22 +1,32 @@
 import * as admin from 'firebase-admin';
 import * as express from 'express';
+import { check, validationResult } from 'express-validator/check';
 
 const BOOK_COLLECTION = 'books';
-const DEFAULT_LIMIT = 20;
-const DEFAULT_PAGE = 0;
+const DEFAULT_LIST_LIMIT = 20;
+const DEFAULT_LIST_PAGE = 0;
 
 const router = express();
 
-router.get('/list', async (request, response) => {
-    const page = request.query.page ? parseInt(request.query.page, 10) : DEFAULT_PAGE;
-    const limit = request.query.limit ? parseInt(request.query.limit, 10) : DEFAULT_LIMIT;
-    const offset = page * limit;
+const listValidation = [
+    check('page').optional().isInt({min: 0}),
+    check('limit').optional().isInt({min: 1, max: 120})
+];
+
+router.get('/list', listValidation, async (req: express.Request, res: express.Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(422).send({ errors: errors.array() });
+    }
+    const limit = req.query.limit ? parseInt(req.query.limit, 10) : DEFAULT_LIST_LIMIT;
+    const page = req.query.page ? parseInt(req.query.page, 10) : DEFAULT_LIST_PAGE;
+    const offset = limit * page;
 
     const snapShot = await admin.firestore().collection(BOOK_COLLECTION).offset(offset).limit(limit).get()
     const data = snapShot.docs.map(doc => {
         return doc.data();
     });
-    response.status(200).send({ data: data });
+    res.status(200).send({ data: data });
 });
 
 export default router
