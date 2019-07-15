81ec661335769b31a05e186bc50755ad8257f87e
Mitu217 <mtksk.26884um@gmail.com>
2019-07-16 00:27:27 +0900

    firebase hosting with typescript

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -1,19 +1,7 @@
-import { NextPage } from 'next';
-import Link from 'next/link';
-import * as React from 'react';
-import Layout from '../components/Layout';
+import App from '../components/App';
 
-const IndexPage: NextPage = () => {
-  return (
-    <Layout title='Home | Next.js + TypeScript Example'>
-      <h1>Welcome to Next.js for TypeScript!</h1>
-      <p>
-        <Link href='/about'>
-          <a>About</a>
-        </Link>
-      </p>
-    </Layout>
-  );
-};
-
-export default IndexPage;
+export default () => (
+  <App>
+    <p>Index Page</p>
+  </App>
+);
